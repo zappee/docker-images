f560b6be3a9f9f717bc494f3790b6c245dc6da12
Arnold SOMOGYI <arnold.somogyi@ext.ec.europa.eu>
2021-09-17 12:31:52 +0200

    create jms resources

--- hello-weblogic-world/oracle-weblogic-admin-server/container-scripts/deploy-hello-weblogic-world.py
+++ hello-weblogic-world/oracle-weblogic-admin-server/container-scripts/deploy-hello-weblogic-world.py
@@ -78,6 +78,74 @@ def createDataSource(_dsName, _dsJNDIName, _dsURL, _dsDriver, _dsUsername, _dsPa
     set('Targets',jarray.array([ObjectName('com.bea:Name=' + _dsTargetName + ',Type=' + _dsTargetType)], ObjectName))
 
 # ------------------------------------------------------------------------------
+# create a new jms-server
+# ------------------------------------------------------------------------------
+def createJmsServer(_jmsServerName, _jmsTargetName, _jmsTargetType):
+    print('creating a new jms-server...')
+    print('   JMS server name: %s' % _jmsServerName)
+    print('   Target:          %s' % _jmsTargetName)
+    print('   Target type:     %s' % _jmsTargetType)
+
+    cd('/')
+    cmo.createJMSServer(_jmsServerName)
+    cd('/JMSServers/' + _jmsServerName)
+    set('Targets', jarray.array([ObjectName('com.bea:Name=' + _jmsTargetName + ',Type=' + _jmsTargetType)], ObjectName))
+
+# ------------------------------------------------------------------------------
+# create a new jms-module
+# ------------------------------------------------------------------------------
+def createJmsModule(_jmsServerName, _jmsModuleName, _jmsModuleSubDeploymentName, _jmsTargetName, _jmsTargetType):
+    print('creating a new jms-module...')
+    print('   JMS server name:                %s' % _jmsServerName)
+    print('   JMS module name:                %s' % _jmsModuleName)
+    print('   JMS module sub-deployment name: %s' % _jmsModuleSubDeploymentName)
+    print('   Target:                         %s' % _jmsTargetName)
+    print('   Target type:                    %s' % _jmsTargetType)
+
+    cd('/')
+    cmo.createJMSSystemResource(_jmsModuleName)
+    cd('/JMSSystemResources/' + _jmsModuleName)
+    set('Targets', jarray.array([ObjectName('com.bea:Name=' + _jmsTargetName + ',Type=' + _jmsTargetType)], ObjectName))
+
+    cmo.createSubDeployment(_jmsModuleSubDeploymentName)
+    cd('/JMSSystemResources/' + _jmsModuleName + '/SubDeployments/' + _jmsModuleSubDeploymentName)
+    set('Targets', jarray.array([ObjectName('com.bea:Name=' + _jmsServerName + ',Type=JMSServer')], ObjectName))
+
+# ------------------------------------------------------------------------------
+# create a new jms-connection-factory
+# ------------------------------------------------------------------------------
+def createConnectionFactory(_jmsModuleName, _jmsModuleSubDeploymentName, _jmsconnectionFactoryName, _jmsconnectionFactoryJndiName):
+    print('creating a new connection-factory...')
+    print('   JMS module name:                %s' % _jmsModuleName)
+    print('   JMS module sub-deployment name: %s' % _jmsModuleSubDeploymentName)
+    print('   Connection factory name:        %s' % _jmsconnectionFactoryName)
+    print('   Connection factory JNDI name:   %s' % _jmsconnectionFactoryJndiName)
+
+    cd('/JMSSystemResources/' + _jmsModuleName + '/JMSResource/' + _jmsModuleName)
+    cmo.createConnectionFactory(_jmsconnectionFactoryName)
+    cd('/JMSSystemResources/' + _jmsModuleName + '/JMSResource/' + _jmsModuleName + '/ConnectionFactories/' + _jmsconnectionFactoryName)
+    cmo.setJNDIName('jms/' + _jmsconnectionFactoryJndiName)
+    cmo.setSubDeploymentName(_jmsModuleSubDeploymentName)
+
+
+# ------------------------------------------------------------------------------
+# create a new distributed-jms-queue
+# ------------------------------------------------------------------------------
+def createDistributedJmsQueue(_jmsModuleName, _jmsModuleSubDeploymentName, _jmsQueueName, _jmsQueueJndiName):
+    print('creating a new distributed-jms-queue...')
+    print('   JMS module name:                %s' % _jmsModuleName)
+    print('   JMS module sub-deployment name: %s' % _jmsModuleSubDeploymentName)
+    print('   JMS queue name:                 %s' % _jmsQueueName)
+    print('   JMS queue JNDI name:            %s' % _jmsQueueJndiName)
+
+    cd('/JMSSystemResources/' + _jmsModuleName + '/JMSResource/' + _jmsModuleName)
+    cmo.createUniformDistributedQueue(_jmsQueueName)
+
+    cd('/JMSSystemResources/' + _jmsModuleName + '/JMSResource/' + _jmsModuleName + '/UniformDistributedQueues/' + _jmsQueueName)
+    cmo.setJNDIName(_jmsQueueJndiName)
+    cmo.setSubDeploymentName(_jmsModuleSubDeploymentName)
+
+# ------------------------------------------------------------------------------
 # main program starts here
 # ------------------------------------------------------------------------------
 adminServerHost = sys.argv[1]
@@ -89,11 +157,6 @@ dbName = sys.argv[6]
 dsUsername = sys.argv[7]
 dsPassword = sys.argv[8]
 
-dsName = 'hello-ds'
-dsJNDIName = 'jdbc/HELLO_DS'
-dsURL = 'jdbc:oracle:thin:@//' + dbHost + ':' + dbPort + '/' + dbName
-dsDriver = 'oracle.jdbc.xa.client.OracleXADataSource'
-
 print('admin server host:     %s' % adminServerHost)
 print('admin server port:     %s' % adminServerPort)
 print('admin server user:     %s' % username)
@@ -102,5 +165,17 @@ print('cluster name:          %s' % clusterName)
 
 connectToServer(adminServerHost, adminServerPort, username, password)
 lockAndEdit()
-createDataSource(dsName, dsJNDIName, dsURL, dsDriver, dsUsername, dsPassword, clusterName, 'Cluster')
+
+dsURL = 'jdbc:oracle:thin:@//' + dbHost + ':' + dbPort + '/' + dbName
+createDataSource('hello-ds', 'jdbc/HELLO_DS', dsURL, 'oracle.jdbc.xa.client.OracleXADataSource', dsUsername, dsPassword, clusterName, 'Cluster')
+
+jmsServerName = 'dev-jms-server'
+jmsModuleName = 'dev-jms-module'
+jmsModuleSubDeploymentName = 'dev-jms-module-subdeployment'
+createJmsServer(jmsServerName, clusterName, 'Cluster')
+createJmsModule(jmsServerName, jmsModuleName, jmsModuleSubDeploymentName, clusterName, 'Cluster')
+createConnectionFactory(jmsModuleName, jmsModuleSubDeploymentName, 'jms-connection-factory', 'jms/qcf')
+createDistributedJmsQueue(jmsModuleName, jmsModuleSubDeploymentName, 'hello-incoming-queue', 'jms/incoming')
+createDistributedJmsQueue(jmsModuleName, jmsModuleSubDeploymentName, 'hello-outgoing-queue', 'jms/outgoing')
+
 saveChanges()
