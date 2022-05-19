cbde52286f50c7340f42aefbb62226a8ff59b676
Arnold SOMOGYI <arnold.somogyi@ext.ec.europa.eu>
2022-05-19 17:37:17 +0200

    improve bash script quality

--- oracle-weblogic/oracle-weblogic-12.2.1.4-managed-server/build.sh
+++ oracle-weblogic/oracle-weblogic-12.2.1.4-managed-server/build.sh
@@ -21,7 +21,7 @@ PUSH_IMAGE=${1:-false}
 docker build --no-cache -t "$DOCKER_REGISTRY/$DOCKER_REGISTRY_NAMESPACE/$IMAGE_NAME":"$IMAGE_VERSION" .
 docker rmi "$(docker image ls -qf dangling=true)"
 
-if [ "$PUSH_IMAGE" = true ] ; then
+if [ "$PUSH_IMAGE" = "true" ] ; then
     echo "pushing the image to the registry..."
     docker push "$DOCKER_REGISTRY/$DOCKER_REGISTRY_NAMESPACE/$IMAGE_NAME":"$IMAGE_VERSION"
 fi
